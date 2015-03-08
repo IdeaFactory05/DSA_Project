6c09647fa1fb348b6cfb7b9577bc42b922552cfc
IdeaFactory05 <bitFact#05>
2015-03-08 11:08:18 +0530

    Uploaded

---
+++ src/realEstate/HouseFile.java
@@ -0,0 +1,5 @@
+package realEstate;
+
+public class HouseFile {
+
+}
---
+++ src/realEstate/RealEstate.java
@@ -0,0 +1,10 @@
+package realEstate;
+
+public class RealEstate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
